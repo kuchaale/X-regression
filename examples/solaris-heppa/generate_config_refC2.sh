cf051162d98cf2e4ac7273c7002deb0527fa4b58
Ales Kuchar <kuchara@mbox.troja.mff.cuni.cz>
2019-07-04 15:27:05 +0000

    Add files via upload

--- examples/solaris-heppa/generate_config_refC2.sh
+++ examples/solaris-heppa/generate_config_refC2.sh
@@ -14,8 +14,8 @@ then
 	spe_reg=spe_for_t_and_u
 elif [ ${variable} = "zmo3,zmnoy" ] || [ ${variable} = "zmnoy,zmo3" ]
 then
-	epp_reg=eep_for_o3_and_noy                                                                                                                      
-	spe_reg=spe_for_o3_and_noy
+	epp_reg=eep_for_noy_and_o3                                                                                                                      
+	spe_reg=spe_for_noy_and_o3
 else
 	echo "none of these options: "${variable}" is valid"
 	exit
@@ -43,7 +43,7 @@ echo "running "${config_name}
 python3 solaris_heppa_analysis-script.py ${config_name}
 
 ens=r3i1p1                                                                                                                                               
-config_name=config_${model}_${sim}${ens}_${variable}_${s_year}-${e_year}.yaml
+config_name=config_${model}_${sim}_${ens}_${variable}_${s_year}-${e_year}.yaml
 variable=${variable} sim=${sim} model=${model} ens=${ens} s_year=${s_year} e_year=${e_year} output_folder=${output_folder} reg_folder=${reg_folder} epp_reg=${epp_reg} spe_reg=${spe_reg} ./templater.sh config_template.yaml > ${config_name}                                                                                                                                   
 echo "running "${config_name}
 python3 solaris_heppa_analysis-script.py ${config_name}
